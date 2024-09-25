37b683c41ba582ba1bc99eb2539deaf6c78b7ddb
Aditya Sharma <aditya10102004@gmail.com>
2024-09-25 21:56:28 +0530

    made startGame() method in class TicTacToe

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.*;
 class TicTacToe {
 
     String[][] board = new String[3][3];
+    Scanner sc = new Scanner(System.in);
 
     public TicTacToe() {
         for (int i = 0; i < 3; i++) {
@@ -29,6 +30,39 @@ class TicTacToe {
         board[row][column] = " O ";
     }
 
+    public void startGame() {
+        int count = 9;
+        int row, column;
+
+        printBoard();
+
+        while (count > 0) {
+
+            if (count % 2 == 0) {
+                row = sc.nextInt();
+                column = sc.nextInt();
+                if (isValidMove(row, column)) {
+                    setStringO(row, column);
+                    printBoard();
+                    count--;
+                } else {
+                    System.out.println("Enter a valid move!");
+                }
+            } else if (count % 2 != 0) {
+                row = sc.nextInt();
+                column = sc.nextInt();
+                if (isValidMove(row, column)) {
+                    setStringX(row, column);
+                    printBoard();
+                    count--;
+                } else {
+                    System.out.println("Enter a valid move!");
+
+                }
+            }
+        }
+    }
+
     private boolean isValidMove(int row, int column) {
         if ((row >= 0 && row < 3) && (column >= 0 && column < 3) && (board[row][column].equals(" - "))) {
             return true;
@@ -41,32 +75,9 @@ class TicTacToe {
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
         TicTacToe tictactoe = new TicTacToe();
-        tictactoe.printBoard();
-
-        int count = 9;
-
-        while (count != 0) {
-
-            if (count % 2 == 0) {
-                int row = sc.nextInt();
-                int column = sc.nextInt();
-                tictactoe.setStringX(row, column);
-                tictactoe.printBoard();
-                count--;
-            } else if (count % 2 != 0) {
-                int row = sc.nextInt();
-                int column = sc.nextInt();
-                tictactoe.setStringO(row, column);
-                tictactoe.printBoard();
-                count--;
-            }
-
-        }
-
-        sc.close();
-
+        tictactoe.startGame();
     }
+
 }
